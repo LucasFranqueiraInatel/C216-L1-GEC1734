0af174db86d8a6aa359413ffac85339eccb9ffba
Lucas Franqueira <lucas.f@gec.inatel.br>
2024-03-09 14:04:35 -0300

    Pratica-0 Done

---
+++ pratica-0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = "Lucas Franqueira - 1734";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
